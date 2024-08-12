919f3fe9b8ed710de039f96560a6659597609d9d
ajaymeru <123156354+ajaymeru@users.noreply.github.com>
2024-08-12 09:56:55 +0530

    footer

--- src/CommonComponents/Footer/Footer.js
+++ src/CommonComponents/Footer/Footer.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import "./Footer.css"
+import logo from "../../CommonComponents/Navbar/Assets/logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
     return (
-        <div>Footer</div>
-    )
+        <div className="footer">
+            <div className="footerbrandlogo">
+                <img src={logo} alt="Brand Logo" />
+            </div>
+            <div className="footersocillogos">
+                <FontAwesomeIcon icon={faInstagram} style={{ color: "#black" }} />
+                <FontAwesomeIcon icon={faFacebookF} style={{ color: "#black" }} />
+                <FontAwesomeIcon icon={faTwitter} style={{ color: "#black" }} />
+            </div>
+            <div className="footercontactus">
+                <h4>Contact Us</h4>
+                <address>
+                    P, 6A, Sriram Nagar Rd,  <br />
+                    opp. Burfi Ghar, Kondapur,  <br />
+                    Hyderabad, Telangana 500084 <br />
+                    <p><a href="tel:+91 81216 70753">81216 70753</a></p>
+                </address>
+            </div>
+        </div>
+    );
 }
 
-export default Footer
+export default Footer;
